046d200eac5e144df60b76c409cd2c8be2ebcbe4
Eric Krause <krause197@gmail.com>
2016-10-05 18:13:37 -0700

    create initial folders and files based off previous project template.  Add gitignore, build.gradle, and database files

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,9 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+public class AnimalTest {
+
+
+  @
---
+++ src/test/java/EndangeredTest.java
@@ -0,0 +1,6 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+public class EndangeredTest {
