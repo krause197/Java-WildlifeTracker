26cefe2d391fc7b84b542de1b863c6bc414a8027
Eric Krause <krause197@gmail.com>
2016-10-06 06:48:17 -0700

    find, all, update, delete, methods to Animal.java

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -56,4 +56,75 @@ public class Animal {
     return gender;
   }
 
+  @Override
+    public boolean equals(Object otherAnimal) {
+      if (!(otherAnimal instanceof Animal)) {
+        return false;
+      } else {
+        Animal newAnimal = (Animal) otherAnimal;
+        return this.getSpecies().equals(newAnimal.getSpecies());
+      }
+    }
+
+    public static Animal find(int id) {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "SELECT * FROM animals WHERE id=:id";
+        Animal animal = con.createQuery(sql)
+          .addParameter("id", id)
+          .executeAndFetchFirst(Animal.class);
+        return animal;
+      }
+    }
+
+    public static List<String> allHealth(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM animal_health";
+      return con.createQuery(sql)
+        .throwOnMappingFailure(false)
+        .executeAndFetch(String.class);
+    }
+  }
+
+    public static List<String> allAge(){
+      try(Connection con = DB.sql2o.open()){
+        String sql = "SELECT * FROM animal_age";
+        return con.createQuery(sql)
+          .throwOnMappingFailure(false)
+          .executeAndFetch(String.class);
+    }
+  }
+
+    public List<AnimalSighting> allSightings(){
+      try(Connection con = DB.sql2o.open()){
+        String sql = "SELECT * FROM sightings WHERE animalid=:id";
+        return con.createQuery(sql)
+          .addParameter("id", this.id)
+          .executeAndFetch(Sighting.class);
+      }
+    }
+
+    public void update(){
+      try(Connection con = DB.sql2o.open()){
+        String sql = "UPDATE animals SET species=:species, health=:health, age=:age, gender=:gender, endangered=:endangered  WHERE id=:id";
+        con.createQuery(sql)
+          .addParameter("species", this.species)
+          .addParameter("health", this.health)
+          .addParameter("age", this.age)
+          .addParameter("gender", this.gender)
+          .addParameter("endangered", this.endangered)
+          .executeUpdate()
+      }
+    }
+
+    public void delete(){
+      try(Connection con = DB.sql2o.open()){
+        String sql = "DELETE FROM animals WHERE id=:id";
+        con.createQuery(sql)
+          .addParameter("id", this.id)
+          .executeUpdate();
+      }
+    }
+
+
+
 }
