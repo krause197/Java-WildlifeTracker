b56a02827dca187f552d79dbabece16b152ca7c3
Eric Krause <krause197@gmail.com>
2016-10-06 07:03:40 -0700

    code for Sighting.java, including getter functions, find, all, update, delete

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -2,154 +2,126 @@ import org.sql2o.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
-// public class ItemRecord {
-//
-//   private int id;
-//   private int patronid;
-//   private int itemid;
-//   private int timesrenewed;
-//   private Timestamp datecheckedout;
-//   private Timestamp datecheckedin;
-//
-//   public static final int MAX_WEEKS_LOANED = 2;
-//   public static final int MAX_TIME_RENEWED = 2;
-//
-//   public ItemRecord(int patronid, int itemid) {
-//     this.patronid = patronid;
-//     this.itemid = itemid;
-//     this.timesrenewed = 0;
-//   }
-//
-//   public int getId(){
-//     return this.id;
-//   }
-//
-//   public int getItemId(){
-//     return this.itemid;
-//   }
-//
-//   public int getPatronId(){
-//     return this.patronid;
-//   }
-//
-//   public long getTimesRenewed(){
-//     long numWeeks = MAX_WEEKS_LOANED * (1+timesrenewed);
-//     return numWeeks;
-//   }
-//
-//   public String getPatronName(){
-//     return Patron.find(this.patronid).getName();
-//   }
-//
-//   public String getDueDate(){
-//
-//     long numWeeks = MAX_WEEKS_LOANED * (1+timesrenewed);
-//     Timestamp time = new Timestamp(datecheckedout.getTime() + (numWeeks * 604800000));
-//     return time.toString();
-//   }
-//
-//   public String getDateCheckedOut(){
-//     return datecheckedout.toString();
-//   }
-//
-//   public String getDateCheckedIn(){
-//     return datecheckedin.toString();
-//   }
-//
-//   public String renewBook(){
-//     if(this.timesrenewed <= MAX_TIME_RENEWED){
-//       timesrenewed++;
-//       try(Connection con = DB.sql2o.open()) {
-//         String sql = "UPDATE itemrecords SET timesrenewed = :timesrenewed WHERE id = :id";
-//           con.createQuery(sql)
-//             .addParameter("timesrenewed", this.timesrenewed)
-//             .addParameter("id", this.id)
-//             .executeUpdate();
-//
-//       }
-//       return "Item has been renewed";
-//     } else
-//       return "Item cannot be renewed";
-//   }
-//
-//   public String getItemTitle(){
-//
-//     if(Book.find(this.itemid).getItemType() == 1){
-//       return Book.find(this.itemid).getTitle();
-//     }
-//     else if(CD.find(this.itemid).getItemType() == 3){
-//       return CD.find(this.itemid).getTitle();
-//     }
-//     else if(Magazine.find(this.itemid).getItemType() == 2){
-//       return Magazine.find(this.itemid).getTitle();
-//     }
-//     else{
-//       return "No Item Found";
-//     }
-//   }
-//
-//   public static ItemRecord find(int id) {
-//       try(Connection con = DB.sql2o.open()) {
-//         String sql = "SELECT * FROM itemrecords WHERE id=:id";
-//         ItemRecord itemrecord = con.createQuery(sql)
-//           .addParameter("id", id)
-//           .executeAndFetchFirst(ItemRecord.class);
-//         return itemrecord;
-//       }
-//     }
-//
-//   // public static List<ItemRecord> findItemRecords(int itemId) {
-//   //   try(Connection con = DB.sql2o.open()) {
-//   //     String sql = "SELECT * FROM itemrecords WHERE itemid=:itemid";
-//   //     List<ItemRecord> itemrecords = con.createQuery(sql)
-//   //       .addParameter("itemid", itemId)
-//   //       .executeAndFetch(ItemRecord.class);
-//   //     return itemrecords;
-//   //   }
-//   // }
-//
-//   // public static List<ItemRecord> findPatronRecords(int patronId) {
-//   //   try(Connection con = DB.sql2o.open()) {
-//   //     String sql = "SELECT * FROM itemrecords WHERE patronid=:patronid";
-//   //     List<ItemRecord> itemrecords = con.createQuery(sql)
-//   //       .addParameter("patronid", patronId)
-//   //       .executeAndFetch(ItemRecord.class);
-//   //     return itemrecords;
-//   //   }
-//   // }
-//
-//
-//   public void checkIn(){
-//       try(Connection con = DB.sql2o.open()) {
-//         String sql = "UPDATE itemrecords SET datecheckedin = now() WHERE id = :id";
-//         con.createQuery(sql).addParameter("id", this.id).executeUpdate();
-//
-//         sql = "UPDATE items SET newestrecord = :crid WHERE id = :itemid";
-//           con.createQuery(sql)
-//             .addParameter("crid", -1)
-//             .addParameter("itemid", this.itemid)
-//             .executeUpdate();
-//
-//       }
-//     }
-//
-//   public void save() {
-//    try(Connection con = DB.sql2o.open()) {
-//     String sql = "INSERT INTO itemrecords (patronid, itemid, datecheckedout, timesrenewed) VALUES (:patronid, :itemid, now(), :timesrenewed)";
-//     this.id = (int) con.createQuery(sql, true)
-//       .addParameter("patronid", this.patronid)
-//       .addParameter("itemid", this.itemid)
-//       .addParameter("timesrenewed", this.timesrenewed)
-//       .executeUpdate()
-//       .getKey();
-//
-//     sql = "UPDATE items SET newestrecord = :crid WHERE id = :itemid";
-//     con.createQuery(sql)
-//       .addParameter("crid", this.id)
-//       .addParameter("itemid", this.itemid)
-//       .executeUpdate();
-//    }
-//  }
-//
-// }
+public class Sighting {
+  private int id;
+  private int animalid;
+  private int rangerid;
+  private int location_d;
+  private Timestamp sighting_date;
+  private String notes;
+
+  public Sighting (int animalid, int rangerid, int locationid, Timestamp sighting_date, String notes){
+    this.animalid = animalid;
+    this.rangerid = rangerid;
+    this.locationid = locationid;
+    this.sighting_date = sighting_date;
+    this.notes = notes;
+
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO sightings (animalid, rangerid, locationid, sighting_date, notes) VALUES (:animalid, :rangerid, :locationid, :sighting_date, :notes);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("animalid", this.animalid)
+        .addParameter("rangerid", this.rangerid)
+        .addParameter("locationid", this.locationid)
+        .addParameter("sighting_date", this.sighting_date)
+        .addParameter("notes", this.notes)
+        .executeUpdate()
+        .getKey();
+  }
+
+  // Getters/Setters
+  public int getId(){
+    return this.id;
+  }
+
+  public int getAnimalId(){
+    return this.animalid;
+  }
+
+  public int getRangerId(){
+    return this.rangerid;
+  }
+
+  public int getLocationId(){
+    return this.locationid;
+  }
+
+  public String getSighting_date(){
+    return new SimpleDateFormat("MMMM dd, yyyy").format(this.sighting_date);
+  }
+
+  public String getNotes(){
+    return this.notes;
+  }
+
+  public String getRangerName(){
+    Ranger ranger = Ranger.find(this.rangerid);
+    return ranger.getName();
+  }
+
+  public String getAnimalSpecies(){
+    Animal animal = Animal.find(this.animalid);
+    return animal.getSpecies();
+  }
+
+  public String getLocationName(){
+    Location location = Location.find(this.locationid);
+    return location.getName();
+  }
+
+  public void updateNotes(String notes){
+    this.notes = notes;
+    this.update();
+  }
+
+  @Override
+  public boolean equals(Object otherSighting){
+    if(!(otherSighting instanceof AnimalSighting)){
+      return false;
+    }else{
+      Sighting newSighting = (Sighting) otherSighting;
+      return newSighting.getId() == this.id;
+    }
+  }
+
+  public static Sighting find(int _id){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM sightings WHERE id=:id";
+      return con.createQuery(sql)
+        .addParameter("id", _id)
+        .executeAndFetchFirst(Sighting.class);
+    }
+  }
+
+  public static List<Sighting> all(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM sightings";
+      return con.createQuery(sql).executeAndFetch(Sighting.class);
+    }
+  }
+
+  public void update(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "UPDATE sightings SET animalid=:animalid, rangerid=:rangerid, locationid=:locationid, sighting_date=:sighting_date, notes=:notes WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("animalid", this.animalid)
+        .addParameter("rangerid", this.rangerid)
+        .addParameter("locationid", this.locationid)
+        .addParameter("sighting_date", this.sighting_date)
+        .addParameter("notes", this.notes)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete(){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM sightings WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+}
